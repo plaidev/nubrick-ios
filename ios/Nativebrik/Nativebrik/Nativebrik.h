feac23d5b3ac49a011da550ec952ae6d1678cd35
RyosukeCla <RyosukeCla@users.noreply.github.com>
2024-02-06 03:03:48 +0900

    Commented out version number and version string in Nativebrik.h

--- ios/Nativebrik/Nativebrik/Nativebrik.h
+++ ios/Nativebrik/Nativebrik/Nativebrik.h
@@ -8,10 +8,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for Nativebrik.
-FOUNDATION_EXPORT double NativebrikVersionNumber;
+//FOUNDATION_EXPORT double NativebrikVersionNumber;
 
 //! Project version string for Nativebrik.
-FOUNDATION_EXPORT const unsigned char NativebrikVersionString[];
+//FOUNDATION_EXPORT const unsigned char NativebrikVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Nativebrik/PublicHeader.h>
 
