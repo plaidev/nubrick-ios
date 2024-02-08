37e4be1ebd0124023071bfaea7c220a2769f3485
RyosukeCla <RyosukeCla@users.noreply.github.com>
2024-02-08 19:25:29 +0900

    add material3

--- android/nativebrik/build.gradle.kts
+++ android/nativebrik/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.compose.runtime:runtime:1.6.0")
     implementation("io.coil-kt:coil:2.5.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("androidx.compose.material3:material3:1.2.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
