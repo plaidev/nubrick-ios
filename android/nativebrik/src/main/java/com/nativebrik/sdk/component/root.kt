270d3f83f4bc8efd7ac6d277997f6b2e446f5744
RyosukeCla <RyosukeCla@users.noreply.github.com>
2024-02-26 16:55:44 +0900

    [android] - use PageBlockData to pass props - support deep link

--- android/nativebrik/src/main/java/com/nativebrik/sdk/component/root.kt
+++ android/nativebrik/src/main/java/com/nativebrik/sdk/component/root.kt
@@ -1,5 +1,8 @@
 package com.nativebrik.sdk.component
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.fadeIn
@@ -24,17 +27,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.nativebrik.sdk.component.provider.container.ContainerProvider
 import com.nativebrik.sdk.component.provider.data.PageDataProvider
 import com.nativebrik.sdk.component.provider.event.EventListenerProvider
+import com.nativebrik.sdk.component.provider.pageblock.PageBlockData
+import com.nativebrik.sdk.component.provider.pageblock.PageBlockProvider
 import com.nativebrik.sdk.component.renderer.ModalBottomSheetBackHandler
 import com.nativebrik.sdk.component.renderer.NavigationHeader
 import com.nativebrik.sdk.component.renderer.Page
 import com.nativebrik.sdk.component.renderer.WebViewPage
 import com.nativebrik.sdk.data.Container
 import com.nativebrik.sdk.schema.PageKind
+import com.nativebrik.sdk.schema.Property
 import com.nativebrik.sdk.schema.UIBlockEventDispatcher
 import com.nativebrik.sdk.schema.UIPageBlock
 import com.nativebrik.sdk.schema.UIRootBlock
@@ -43,9 +50,10 @@ import kotlinx.coroutines.launch
 
 internal class RootViewModel: ViewModel {
     private val root: UIRootBlock
-    private val pages: List<UIPageBlock>;
-    val displayedPageBlock = mutableStateOf<UIPageBlock?>(null)
-    val modalStack = mutableStateOf<List<UIPageBlock>>(listOf())
+    private val pages: List<UIPageBlock>
+    private val context: Context
+    val displayedPageBlock = mutableStateOf<PageBlockData?>(null)
+    val modalStack = mutableStateOf<List<PageBlockData>>(listOf())
     val displayedModalIndex = mutableIntStateOf(-1)
     val modalVisibility = mutableStateOf(false)
     val webviewUrl = mutableStateOf("")
@@ -60,7 +68,9 @@ internal class RootViewModel: ViewModel {
         scope: CoroutineScope,
         sheetState: SheetState,
         onDismiss: ((root: UIRootBlock) -> Unit) = {},
+        context: Context,
     ) {
+        this.context = context
         this.root = root
         this.onDismiss = onDismiss
         this.scope = scope
@@ -84,7 +94,25 @@ internal class RootViewModel: ViewModel {
         this.render(destId)
     }
 
-    fun render(destId: String) {
+    fun handleUIEvent(it: UIBlockEventDispatcher) {
+        val destId = it.destinationPageId ?: ""
+        val deepLink = it.deepLink ?: ""
+        if (destId.isNotEmpty()) {
+            this.render(destId)
+        } else if (deepLink.isNotEmpty()) {
+            this.openDeepLink(deepLink)
+        }
+    }
+
+    private fun openDeepLink(link: String) {
+        val data = Uri.parse(link) ?: return
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            this.data = data
+        }
+        this.context.startActivity(intent)
+    }
+
+    private fun render(destId: String, properties: List<Property>? = null) {
         val destBlock = this.pages.firstOrNull {
             it.id == destId
         }
@@ -105,7 +133,7 @@ internal class RootViewModel: ViewModel {
 
         if (destBlock.data?.kind == PageKind.MODAL) {
             val index = this.modalStack.value.indexOfFirst {
-                it.id == destId
+                it.block.id == destId
             }
             if (index > 0) {
                 // if it's already in modal stack, jump to the target stack
@@ -113,9 +141,9 @@ internal class RootViewModel: ViewModel {
                 return
             }
 
-            val modalStack = mutableListOf<UIPageBlock>()
+            val modalStack = mutableListOf<PageBlockData>()
             modalStack.addAll(this.modalStack.value)
-            modalStack.add(destBlock)
+            modalStack.add(PageBlockData(destBlock, properties))
             this.modalStack.value = modalStack
             this.displayedModalIndex.intValue = modalStack.size - 1
             this.modalVisibility.value = true
@@ -123,7 +151,7 @@ internal class RootViewModel: ViewModel {
         }
 
         this.dismiss()
-        this.displayedPageBlock.value = destBlock
+        this.displayedPageBlock.value = PageBlockData(destBlock, properties)
     }
 
     fun back() {
@@ -170,13 +198,14 @@ internal class RootViewModel: ViewModel {
 @Composable
 internal fun ModalPage(
     container: Container,
-    listener: (event: UIBlockEventDispatcher) -> Unit,
-    block: UIPageBlock,
+    blockData: PageBlockData,
     modifier: Modifier = Modifier
 ) {
-    PageDataProvider(container = container, request = block.data?.httpRequest) {
-        EventListenerProvider(listener = listener) {
-            Page(block = block, modifier)
+    PageBlockProvider(
+        blockData,
+    ) {
+        PageDataProvider(container = container, request = blockData.block.data?.httpRequest) {
+            Page(block = blockData.block, modifier)
         }
     }
 }
@@ -190,22 +219,20 @@ internal fun Root(
     embeddingVisibility: Boolean = true,
     onDismiss: ((root: UIRootBlock) -> Unit) = {},
 ) {
+    val context = LocalContext.current
     val scrollState = rememberScrollState()
     val webviewSheetState = rememberModalBottomSheetState()
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
-    val viewModel = remember(root, sheetState, scope, onDismiss) {
-        RootViewModel(root, scope, sheetState, onDismiss)
+    val viewModel = remember(root, sheetState, scope, onDismiss, context) {
+        RootViewModel(root, scope, sheetState, onDismiss, context)
     }
     val bottomSheetProps = remember {
         ModalBottomSheetDefaults.properties(shouldDismissOnBackPress = false)
     }
     val listener = remember<(event: UIBlockEventDispatcher) -> Unit>(key1 = viewModel) {
-        {
-            val destId = it.destinationPageId
-            if (destId != null) {
-                viewModel.render(destId)
-            }
+        return@remember {
+            viewModel.handleUIEvent(it)
         }
     }
 
@@ -213,73 +240,75 @@ internal fun Root(
     val modalStack = viewModel.modalStack.value
 
     ContainerProvider(container = container) {
-        Box {
-            if (embeddingVisibility && displayedPageBlock != null) {
-                AnimatedContent(
-                    targetState = displayedPageBlock,
-                    transitionSpec = {
-                        fadeIn() togetherWith fadeOut()
-                    },
-                    label = "Embedding",
-                ) {
-                    PageDataProvider(container = container, request = it.data?.httpRequest) {
-                        EventListenerProvider(listener = listener) {
-                            Page(block = it, modifier)
+        EventListenerProvider(listener = listener) {
+            Box {
+                if (embeddingVisibility && displayedPageBlock != null) {
+                    AnimatedContent(
+                        targetState = displayedPageBlock,
+                        transitionSpec = {
+                            fadeIn() togetherWith fadeOut()
+                        },
+                        label = "Embedding",
+                    ) {
+                        PageBlockProvider(it) {
+                            PageDataProvider(container = container, request = it.block.data?.httpRequest) {
+                                Page(block = it.block, modifier)
+                            }
                         }
                     }
                 }
-            }
-            if (viewModel.modalVisibility.value) {
-                BackHandler(true) {
-                    viewModel.back()
-                }
-                ModalBottomSheet(
-                    sheetState = sheetState,
-                    onDismissRequest = {
-                        viewModel.handleModalDismiss()
-                    },
-                    properties = bottomSheetProps,
-                ) {
-                    ModalBottomSheetBackHandler {
+                if (viewModel.modalVisibility.value) {
+                    BackHandler(true) {
                         viewModel.back()
                     }
-                    Column {
-                        AnimatedContent(
-                            targetState = viewModel.displayedModalIndex.intValue,
-                            transitionSpec = {
-                                if (targetState > initialState) {
-                                    slideInHorizontally { it } togetherWith slideOutHorizontally { -it } + fadeOut()
-                                } else {
-                                    slideInHorizontally { -it } togetherWith slideOutHorizontally { it } + fadeOut()
-                                }
-                            },
-                            label = "Bottom Sheet"
-                        ) {
-                            val stack = modalStack[it]
-                            NavigationHeader(it, stack, onClose = { viewModel.close() }, onBack = { viewModel.back() })
-                            ModalPage(
-                                container = container,
-                                listener = listener,
-                                block = stack,
-                            )
+                    ModalBottomSheet(
+                        sheetState = sheetState,
+                        onDismissRequest = {
+                            viewModel.handleModalDismiss()
+                        },
+                        properties = bottomSheetProps,
+                    ) {
+                        ModalBottomSheetBackHandler {
+                            viewModel.back()
+                        }
+                        Column {
+                            AnimatedContent(
+                                targetState = viewModel.displayedModalIndex.intValue,
+                                transitionSpec = {
+                                    if (targetState > initialState) {
+                                        slideInHorizontally { it } togetherWith slideOutHorizontally { -it } + fadeOut()
+                                    } else {
+                                        slideInHorizontally { -it } togetherWith slideOutHorizontally { it } + fadeOut()
+                                    }
+                                },
+                                label = "Bottom Sheet"
+                            ) {
+                                val stack = modalStack[it]
+                                NavigationHeader(it, stack.block, onClose = { viewModel.close() }, onBack = { viewModel.back() })
+                                ModalPage(
+                                    container = container,
+                                    blockData = stack,
+                                )
+                            }
                         }
                     }
                 }
-            }
-            if (viewModel.webviewUrl.value.isNotEmpty()) {
-                ModalBottomSheet(
-                    sheetState = webviewSheetState,
-                    onDismissRequest = {
-                        viewModel.handleWebviewDismiss()
-                    }) {
-                    Box(
-                        Modifier.verticalScroll(scrollState, true)
-                    ) {
-                        WebViewPage(url = viewModel.webviewUrl.value, modifier = Modifier.defaultMinSize(minHeight = 200f.dp))
+                if (viewModel.webviewUrl.value.isNotEmpty()) {
+                    ModalBottomSheet(
+                        sheetState = webviewSheetState,
+                        onDismissRequest = {
+                            viewModel.handleWebviewDismiss()
+                        }) {
+                        Box(
+                            Modifier.verticalScroll(scrollState, true)
+                        ) {
+                            WebViewPage(url = viewModel.webviewUrl.value, modifier = Modifier.defaultMinSize(minHeight = 200f.dp))
+                        }
                     }
                 }
             }
         }
+
     }
 
 }
