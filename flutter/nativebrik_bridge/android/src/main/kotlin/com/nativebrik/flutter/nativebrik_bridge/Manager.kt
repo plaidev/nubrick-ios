c2bbdd8ff9ad502b34411373e447978dd434958d
RyosukeCla <RyosukeCla@users.noreply.github.com>
2024-03-19 17:25:30 +0900

    [flutter] impl android side code for flutter sdk

---
+++ flutter/nativebrik_bridge/android/src/main/kotlin/com/nativebrik/flutter/nativebrik_bridge/Manager.kt
@@ -0,0 +1,208 @@
+package com.nativebrik.flutter.nativebrik_bridge
+
+import android.content.Context
+import com.nativebrik.sdk.NativebrikClient
+import com.nativebrik.sdk.__DO_NOT_USE_THIS_INTERNAL_BRIDGE
+import com.nativebrik.sdk.data.NotFoundException
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+import io.flutter.plugin.common.StandardMessageCodec
+import android.view.View
+import android.widget.LinearLayout
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import com.nativebrik.sdk.NativebrikEvent
+import com.nativebrik.sdk.remoteconfig.RemoteConfigVariant
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+internal data class ConfigEntity(val variant: RemoteConfigVariant?, val experimentId: String?)
+
+internal class NativebrikBridgeManager(private val binaryMessenger: BinaryMessenger) {
+    private var nativebrikClient: NativebrikClient? = null
+    private var bridgeClient: __DO_NOT_USE_THIS_INTERNAL_BRIDGE? = null
+
+    private var embeddingMap: MutableMap<String, Any?> = mutableMapOf()
+    private var configMap: MutableMap<String, ConfigEntity> = mutableMapOf()
+
+    fun setNativebrikClient(client: NativebrikClient) {
+        this.nativebrikClient = client
+        this.bridgeClient = __DO_NOT_USE_THIS_INTERNAL_BRIDGE(client)
+    }
+
+    // embedding
+    @OptIn(DelicateCoroutinesApi::class)
+    fun connectEmbedding(channelId: String, experimentId: String, componentId: String? = null) {
+        val methodChannel = MethodChannel(this.binaryMessenger, "Nativebrik/Embedding/$channelId")
+        GlobalScope.launch(Dispatchers.IO) {
+            val result = bridgeClient?.connectEmbedding(experimentId, componentId)
+            if (result == null) {
+                GlobalScope.launch(Dispatchers.Main) {
+                    methodChannel.invokeMethod(EMBEDDING_PHASE_UPDATE_METHOD, "not-found")
+                }
+                return@launch
+            }
+            result.onSuccess {
+                embeddingMap[channelId] = it
+                GlobalScope.launch(Dispatchers.Main) {
+                    methodChannel.invokeMethod(EMBEDDING_PHASE_UPDATE_METHOD, "completed")
+                }
+            }.onFailure {
+                when (it) {
+                    is NotFoundException -> {
+                        GlobalScope.launch(Dispatchers.Main) {
+                            methodChannel.invokeMethod(EMBEDDING_PHASE_UPDATE_METHOD, "not-found")
+                        }
+                    }
+                    else -> {
+                        GlobalScope.launch(Dispatchers.Main) {
+                            methodChannel.invokeMethod(EMBEDDING_PHASE_UPDATE_METHOD, "failed")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun disconnectEmbedding(channelId: String) {
+        embeddingMap.remove(channelId)
+    }
+
+    @Composable
+    fun Render(channelId: String, modifier: Modifier = Modifier) {
+        if (channelId.isEmpty()) {
+            return
+        }
+        val bridgeClient = this.bridgeClient ?: return
+        val methodChannel = remember(channelId) {
+            MethodChannel(this.binaryMessenger, "Nativebrik/Embedding/$channelId")
+        }
+        val data = this.embeddingMap[channelId]
+        bridgeClient.render(modifier, data, onEvent = { event ->
+            methodChannel.invokeMethod(ON_EVENT_METHOD, mapOf(
+                "name" to event.name,
+                "deepLink" to event.deepLink,
+                "payload" to event.payload?.map { prop ->
+                    mapOf(
+                        "name" to prop.name,
+                        "value" to prop.value,
+                        "type" to prop.type,
+                    )
+                }
+            ))
+        })
+    }
+
+    @Composable
+    fun RenderOverlay() {
+        nativebrikClient?.experiment?.Overlay()
+    }
+
+    // remote config
+    suspend fun connectRemoteConfig(channelId: String, experimentId: String): Result<String> {
+        if (channelId.isEmpty()) {
+            return Result.success("not-found")
+        }
+        this.configMap[channelId] = ConfigEntity(null, null)
+
+        if (experimentId.isEmpty()) {
+            return Result.success("not-found")
+        }
+        val client = this.nativebrikClient ?: return Result.success("not-found")
+        val config = client.experiment.remoteConfig(experimentId)
+        val variant = config.fetch().getOrElse {
+            val status = when (it) {
+                is NotFoundException -> "not-found"
+                else -> "failed"
+            }
+            return Result.success(status)
+        }
+        if (this.configMap[channelId] != null) {
+            this.configMap[channelId] = ConfigEntity(variant, variant.experimentId)
+        }
+        return Result.success("competed")
+    }
+
+    fun disconnectRemoteConfig(channelId: String) {
+        this.configMap.remove(channelId)
+    }
+
+    fun getRemoteConfigValue(channelId: String, key: String): String? {
+        if (channelId.isEmpty()) return null
+        if (key.isEmpty()) return null
+        val config = this.configMap[channelId] ?: return null
+        val variant = config.variant ?: return null
+        return variant.get(key)
+    }
+
+    fun connectEmbeddingInRemoteConfigValue(channelId: String, key: String, embeddingChannelId: String) {
+        if (channelId.isEmpty()) return
+        val config = this.configMap[channelId] ?: return
+        val variant = config.variant ?: return
+        val componentId = variant.get(key) ?: return
+        val experimentId = config.experimentId ?: return
+        this.connectEmbedding(embeddingChannelId, experimentId, componentId)
+    }
+
+    fun dispatch(name: String) {
+        this.nativebrikClient?.experiment?.dispatch(NativebrikEvent(name))
+    }
+}
+
+internal class OverlayViewFactory(private val manager: NativebrikBridgeManager): PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        return OverlayView(context, manager)
+    }
+}
+
+internal class OverlayView(context: Context, manager: NativebrikBridgeManager): PlatformView {
+    private val view: ComposeView
+
+    override fun getView(): View {
+        return view
+    }
+
+    override fun dispose() {}
+
+    init {
+        view = ComposeView(context).apply {
+            setContent {
+                manager.RenderOverlay()
+            }
+        }
+    }
+}
+
+internal class NativeViewFactory(private val manager: NativebrikBridgeManager): PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        val creationParams = args as Map<*, *>?
+        val channelId = creationParams?.get("channelId") as String
+        return NativeView(context, channelId, manager)
+    }
+}
+
+internal class NativeView(context: Context, channelId: String, manager: NativebrikBridgeManager): PlatformView {
+    private val view: ComposeView
+
+    override fun getView(): View {
+        return view
+    }
+
+    override fun dispose() {}
+
+    init {
+        val param = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        view = ComposeView(context).apply {
+            setContent {
+                manager.Render(channelId)
+            }
+            layoutParams = param
+        }
+    }
+}
--- flutter/nativebrik_bridge/android/src/main/kotlin/com/nativebrik/flutter/nativebrik_bridge/NativebrikBridgePlugin.kt
+++ flutter/nativebrik_bridge/android/src/main/kotlin/com/nativebrik/flutter/nativebrik_bridge/NativebrikBridgePlugin.kt
@@ -1,12 +1,24 @@
 package com.nativebrik.flutter.nativebrik_bridge
 
-import androidx.annotation.NonNull
+import android.content.Context
+import com.nativebrik.sdk.Config
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import com.nativebrik.sdk.VERSION
+import com.nativebrik.sdk.NativebrikClient
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+internal const val EMBEDDING_VIEW_ID = "nativebrik-embedding-view"
+internal const val OVERLAY_VIEW_ID = "nativebrik-overlay-view"
+internal const val EMBEDDING_PHASE_UPDATE_METHOD = "embedding-phase-update"
+internal const val ON_EVENT_METHOD = "on-event"
 
 /** NativebrikBridgePlugin */
 class NativebrikBridgePlugin: FlutterPlugin, MethodCallHandler {
@@ -15,15 +27,77 @@ class NativebrikBridgePlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var context: Context
+  private lateinit var manager: NativebrikBridgeManager
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "nativebrik_bridge")
+    val messenger = flutterPluginBinding.binaryMessenger
+    manager = NativebrikBridgeManager(messenger)
+    context = flutterPluginBinding.applicationContext
+    channel = MethodChannel(messenger, "nativebrik_bridge")
     channel.setMethodCallHandler(this)
+
+    flutterPluginBinding.platformViewRegistry.registerViewFactory(
+      OVERLAY_VIEW_ID,
+      OverlayViewFactory(manager)
+    )
+    flutterPluginBinding.platformViewRegistry.registerViewFactory(
+      EMBEDDING_VIEW_ID,
+      NativeViewFactory(manager)
+    )
   }
 
+  @OptIn(DelicateCoroutinesApi::class)
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "getNativebrikSDKVersion") {
+      result.success(VERSION)
+    } else if (call.method == "connectClient") {
+      val projectId = call.arguments as String
+      if (projectId.isEmpty()) {
+        result.success("ok")
+        return
+      }
+      val client = NativebrikClient(Config(projectId), context)
+      this.manager.setNativebrikClient(client)
+      result.success("ok")
+    } else if (call.method == "connectEmbedding") {
+      val channelId = call.argument<String>("channelId") as String
+      val id = call.argument<String>("id") as String
+      this.manager.connectEmbedding(channelId, id)
+      result.success("ok")
+    } else if (call.method == "disconnectEmbedding") {
+      val channelId = call.arguments as String
+      this.manager.disconnectEmbedding(channelId)
+      result.success("ok")
+    } else if (call.method == "connectRemoteConfig") {
+      val channelId = call.argument<String>("channelId") as String
+      val id = call.argument<String>("id") as String
+      GlobalScope.launch(Dispatchers.IO) {
+          manager.connectRemoteConfig(channelId, id).onSuccess {
+            result.success(it)
+          }.onFailure {
+            result.success("failed")
+          }
+      }
+    } else if (call.method == "disconnectRemoteConfig") {
+      val channelId = call.arguments as String
+      this.manager.disconnectRemoteConfig(channelId)
+      result.success("ok")
+    } else if (call.method == "getRemoteConfigValue") {
+      val channelId = call.argument<String>("channelId") as String
+      val key = call.argument<String>("key") as String
+      val value = this.manager.getRemoteConfigValue(channelId, key)
+      result.success(value)
+    } else if (call.method == "connectEmbeddingInRemoteConfigValue") {
+      val channelId = call.argument<String>("channelId") as String
+      val embeddingChannelId = call.argument<String>("embeddingChannelId") as String
+      val key = call.argument<String>("key") as String
+      this.manager.connectEmbeddingInRemoteConfigValue(channelId, key, embeddingChannelId)
+      result.success("ok")
+    } else if (call.method == "dispatch") {
+      val event = call.arguments as String
+      this.manager.dispatch(event)
+      result.success("ok")
     } else {
       result.notImplemented()
     }
