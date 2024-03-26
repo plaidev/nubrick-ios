e56dbcedb4b75f6eef11ea6ba3c0a3e6d2afc9ba
RyosukeCla <RyosukeCla@users.noreply.github.com>
2024-03-26 19:29:37 +0900

    [ios] Update Nativebrik version to 0.5.5

--- ios/Nativebrik/Nativebrik/sdk.swift
+++ ios/Nativebrik/Nativebrik/sdk.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 import Combine
 
-public let nativebrikSdkVersion = "0.5.4"
+public let nativebrikSdkVersion = "0.5.5"
 public let isNativebrikAvailable: Bool = {
     if #available(iOS 15.0, *) {
         return true
