b4a7ed56c68acdb5bffbfc6517d851d41b887e5f
RyosukeCla <RyosukeCla@users.noreply.github.com>
2025-02-28 16:31:32 +0900

    feat(flutter): Export public APIs for Nativebrik bridge package
    - Add exports for key Nativebrik bridge modules
    - Improve package accessibility by exposing core classes and interfaces
    - Enable easier import and usage of Nativebrik bridge functionalities

--- flutter/nativebrik_bridge/lib/nativebrik_bridge.dart
+++ flutter/nativebrik_bridge/lib/nativebrik_bridge.dart
@@ -3,6 +3,14 @@ import 'package:nativebrik_bridge/embedding.dart';
 import 'package:nativebrik_bridge/utils/parse_event.dart';
 import 'channel/nativebrik_bridge_platform_interface.dart';
 
+// Export public APIs
+export 'package:nativebrik_bridge/crash_report.dart';
+export 'package:nativebrik_bridge/dispatcher.dart';
+export 'package:nativebrik_bridge/embedding.dart';
+export 'package:nativebrik_bridge/provider.dart';
+export 'package:nativebrik_bridge/remote_config.dart';
+export 'package:nativebrik_bridge/user.dart';
+
 /// A bridge client to the nativebrik SDK.
 ///
 /// - Initialize the bridge with the project ID before using nativebrik SDK.
