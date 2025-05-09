27fe88345c34585057075c00ab6f083330ab20e5
t-jimbo <jbear.um@gmail.com>
2025-05-09 10:44:44 +0900

    fix: enable back history state

--- android/nativebrik/src/main/java/com/nativebrik/sdk/component/renderer/webview.kt
+++ android/nativebrik/src/main/java/com/nativebrik/sdk/component/renderer/webview.kt
@@ -3,13 +3,18 @@ package com.nativebrik.sdk.component.renderer
 import android.annotation.SuppressLint
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun WebViewPage(url: String, modifier: Modifier = Modifier) {
+fun WebViewPage(url: String, onDismiss: () -> Unit, modifier: Modifier = Modifier) {
+    val webViewState = remember { mutableStateOf<WebView?>(null) }
+
     AndroidView(
         modifier = modifier,
         factory = { context ->
@@ -19,10 +24,23 @@ fun WebViewPage(url: String, modifier: Modifier = Modifier) {
                 this.settings.useWideViewPort = true
                 this.settings.setSupportZoom(true)
                 this.isVerticalScrollBarEnabled = true
+                loadUrl(url)
+                webViewState.value = this
             }
         },
         update = { webView ->
             webView.loadUrl(url)
         }
     )
+
+    BackHandler(enabled = true) {
+        val webView = webViewState.value
+        if (webView != null && webView.canGoBack()) {
+            // history back
+            webView.goBack()
+        } else {
+            // close dialog
+            onDismiss()
+        }
+    }
 }
