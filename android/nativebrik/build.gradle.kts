c88d2e3f11801b34795c95ec9acf02b8d3009341
RyosukeCla <RyosukeCla@users.noreply.github.com>
2025-05-14 18:21:56 +0900

    chore: bump version to 0.4.4 in build.gradle.kts and sdk.kt

--- android/nativebrik/build.gradle.kts
+++ android/nativebrik/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.nativebrik"
-version = "0.4.3"
+version = "0.4.4"
 
 android {
     namespace = "com.nativebrik.sdk"
--- android/nativebrik/src/main/java/com/nativebrik/sdk/sdk.kt
+++ android/nativebrik/src/main/java/com/nativebrik/sdk/sdk.kt
@@ -33,7 +33,7 @@ import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
-const val VERSION = "0.4.3"
+const val VERSION = "0.4.4"
 
 data class Endpoint(
     val cdn: String = "https://cdn.nativebrik.com",
