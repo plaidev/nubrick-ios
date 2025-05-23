5f563d600a0f4d277a66186e8cf823ffa8012ddd
RyosukeCla <RyosukeCla@users.noreply.github.com>
2025-05-23 11:39:02 +0900

    chore: update to version 0.8.0, add Product Tour feature, and update SDK to 0.4.8; modify AppDelegate for Swift 5.3 compatibility

--- flutter/nativebrik_bridge/example/ios/Runner/AppDelegate.swift
+++ flutter/nativebrik_bridge/example/ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 import Nativebrik
 import YogaKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
