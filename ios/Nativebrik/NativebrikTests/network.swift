f86747387d0331e98bca38b063d0827e689db936
t-jimbo <jbear.um@gmail.com>
2025-05-26 10:36:01 +0900

    test

--- ios/Nativebrik/NativebrikTests/network.swift
+++ ios/Nativebrik/NativebrikTests/network.swift
@@ -26,7 +26,7 @@ final class CacheStoreTests: XCTestCase {
         cache = CacheStore(policy: NativebrikCachePolicy())
         let data = "testData".data(using: .utf8)!
         cache.set(key: "testKey", data: data)
-        __for_test_sync_datetime_offset(offset: 24 * 60 * 60 * 1000)
+        __for_test_sync_datetime_offset(offset: 33 * 60 * 60 * 1000)
         let cacheObject = cache.get(key: "testKey")
         XCTAssertNil(cacheObject)
     }
@@ -45,7 +45,7 @@ final class GetDataTest: XCTestCase {
     func testGetDataAndCached() async {
         let cache = CacheStore(policy: NativebrikCachePolicy(staleTime: 60))
         let url = URL(string: "https://example.com")!
-        await getData(url: url, cache: cache)
+        let _ = await getData(url: url, cache: cache)
         let cached = cache.get(key: url.absoluteString)
         XCTAssertNotNil(cached)
         XCTAssertEqual(cached?.isStale(), false)
