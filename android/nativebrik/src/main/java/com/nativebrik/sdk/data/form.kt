0a1df204c01983094eba4ba0291da96fcf02732b
t-jimbo <jbear.um@gmail.com>
2025-05-28 16:16:17 +0900

    feat(android): implement add listener

--- android/nativebrik/src/main/java/com/nativebrik/sdk/data/form.kt
+++ android/nativebrik/src/main/java/com/nativebrik/sdk/data/form.kt
@@ -2,32 +2,35 @@ package com.nativebrik.sdk.data
 
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.JsonPrimitive
 
 internal sealed class FormValue {
-    class Bool(val bool: Boolean): FormValue()
-    class StrList(val list: List<String>): FormValue()
-    class Str(val str: String): FormValue()
+    class Bool(val bool: Boolean) : FormValue()
+    class StrList(val list: List<String>) : FormValue()
+    class Str(val str: String) : FormValue()
 
     fun toJsonElement(): JsonElement {
         return when (this) {
             is Bool -> JsonPrimitive(this.bool)
             is Str -> JsonPrimitive(this.str)
             is StrList -> JsonArray(this.list.map { JsonPrimitive(it) })
-            else -> JsonNull
         }
     }
 }
+
+typealias FormValueListener = (values: Map<String, JsonElement>) -> Unit
+
 
 internal interface FormRepository {
     fun getFormData(): Map<String, JsonElement>
     fun setValue(key: String, value: FormValue)
     fun getValue(key: String): FormValue?
+    fun addListener(listener: FormValueListener)
 }
 
-internal class FormRepositoryImpl: FormRepository {
+internal class FormRepositoryImpl : FormRepository {
     private val map: MutableMap<String, FormValue> = mutableMapOf()
+    private val listeners: MutableList<FormValueListener> = mutableListOf()
 
     override fun getFormData(): Map<String, JsonElement> {
         return this.map.entries.associate { it.key to it.value.toJsonElement() }
@@ -39,5 +42,13 @@ internal class FormRepositoryImpl: FormRepository {
 
     override fun setValue(key: String, value: FormValue) {
         this.map[key] = value
+        val values = this.getFormData()
+        listeners.forEach { listener ->
+            listener(values)
+        }
+    }
+
+    override fun addListener(listener: FormValueListener) {
+        this.listeners.add(listener)
     }
 }
