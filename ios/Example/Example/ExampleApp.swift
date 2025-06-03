b4ecb4fbab27b12002709e7632aa972147c47af9
t-jimbo <jbear.um@gmail.com>
2025-06-03 13:30:31 +0900

    fmt

--- ios/Example/Example/ExampleApp.swift
+++ ios/Example/Example/ExampleApp.swift
@@ -10,46 +10,46 @@ import SwiftUI
 import UIKit
 
 let nativebrik = {
-	guard let projectId = Bundle.main.object(forInfoDictionaryKey: "PROJECT_ID") as? String else {
-		fatalError("Missing or invalid PROJECT_ID in Info.plist")
-	}
+    guard let projectId = Bundle.main.object(forInfoDictionaryKey: "PROJECT_ID") as? String else {
+        fatalError("Missing or invalid PROJECT_ID in Info.plist")
+    }
 
-	if let cdnUrl = Bundle.main.object(forInfoDictionaryKey: "CDN_URL") as? String {
-		nativebrikCdnUrl = cdnUrl
-	}
-	if let trackUrl = Bundle.main.object(forInfoDictionaryKey: "TRACK_URL") as? String {
-		nativebrikTrackUrl = trackUrl
-	}
+    if let cdnUrl = Bundle.main.object(forInfoDictionaryKey: "CDN_URL") as? String {
+        nativebrikCdnUrl = cdnUrl
+    }
+    if let trackUrl = Bundle.main.object(forInfoDictionaryKey: "TRACK_URL") as? String {
+        nativebrikTrackUrl = trackUrl
+    }
 
-	return NativebrikClient(
-		projectId: projectId,
-		cachePolicy: NativebrikCachePolicy(cacheTime: 10 * 60, staleTime: 0)
-	)
+    return NativebrikClient(
+        projectId: projectId,
+        cachePolicy: NativebrikCachePolicy(cacheTime: 10 * 60, staleTime: 0)
+    )
 }()
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-	func application(
-		_ application: UIApplication,
-		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-	) -> Bool {
-		NSSetUncaughtExceptionHandler { exception in
-			nativebrik.experiment.record(exception: exception)
-		}
-		return true
-	}
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        NSSetUncaughtExceptionHandler { exception in
+            nativebrik.experiment.record(exception: exception)
+        }
+        return true
+    }
 }
 
 @main
 struct ExampleApp: App {
-	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-	var body: some Scene {
-		WindowGroup {
-			NativebrikProvider(
-				client: nativebrik
-			) {
-				ContentView()
-			}
-		}
-	}
+    var body: some Scene {
+        WindowGroup {
+            NativebrikProvider(
+                client: nativebrik
+            ) {
+                ContentView()
+            }
+        }
+    }
 }
