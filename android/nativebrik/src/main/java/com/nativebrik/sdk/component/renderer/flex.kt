c47c4e2dda5bb059aeb4416eb65be0e530a4c866
t-jimbo <jbear.um@gmail.com>
2025-06-17 21:36:34 +0900

    refactor: default 0.dp

--- android/nativebrik/src/main/java/com/nativebrik/sdk/component/renderer/flex.kt
+++ android/nativebrik/src/main/java/com/nativebrik/sdk/component/renderer/flex.kt
@@ -272,10 +272,10 @@ internal fun Modifier.frameSize(frame: FrameData?): Modifier {
 
 
 @Composable
-internal fun Modifier.framePadding(frame: FrameData?, insetTop: Dp? = null): Modifier {
+internal fun Modifier.framePadding(frame: FrameData?, insetTop: Dp = 0.dp): Modifier {
     return this.padding(
         start = frame?.paddingLeft?.dp ?: 0.dp,
-        top = (frame?.paddingTop?.dp ?: 0.dp) + (insetTop ?: 0.dp),
+        top = (frame?.paddingTop?.dp ?: 0.dp) + insetTop,
         end = frame?.paddingRight?.dp ?: 0.dp,
         bottom = frame?.paddingBottom?.dp ?: 0.dp,
     )
@@ -368,7 +368,7 @@ internal fun parseVerticalJustifyContent(
 internal fun Flex(
     block: UIFlexContainerBlock,
     modifier: Modifier = Modifier,
-    insetTop: Dp?
+    insetTop: Dp
 ) {
     val data = DataContext.state
     val direction: FlexDirection = block.data?.direction ?: FlexDirection.ROW
