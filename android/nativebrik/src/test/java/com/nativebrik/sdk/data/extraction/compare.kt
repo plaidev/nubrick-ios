c95367c490c22817e59d483afb918bba385f0a16
RyosukeCla <RyosukeCla@users.noreply.github.com>
2025-06-27 14:28:53 +0900

    refactor: remove CompareTest class and update ComparisonUnitTest to include boolean comparisons and null handling

--- android/nativebrik/src/test/java/com/nativebrik/sdk/data/extraction/compare.kt
+++ android/nativebrik/src/test/java/com/nativebrik/sdk/data/extraction/compare.kt
@@ -1,8 +1,8 @@
 package com.nativebrik.sdk.data.extraction
 
 import com.nativebrik.sdk.data.user.UserProperty
-import com.nativebrik.sdk.data.user.UserPropertyType
 import com.nativebrik.sdk.schema.ConditionOperator
+import com.nativebrik.sdk.schema.UserPropertyType
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -12,65 +12,91 @@ class ComparisonUnitTest {
     private val doubleProp = UserProperty(name = "double", value = "12.3", type = UserPropertyType.DOUBLE)
     private val semverProp = UserProperty(name = "semver", value = "1.1.1", type = UserPropertyType.SEMVER)
     private val timeProp = UserProperty(name = "time", value = "2011-10-05T14:48:00.000Z", type = UserPropertyType.TIMESTAMPZ)
+    private val boolProp = UserProperty(name = "bool", value = "false", type = UserPropertyType.BOOLEAN)
+
+    @Test
+    fun shouldCompareWithPropTypeOverride_asType() {
+        assertEquals(true, comparePropWithConditionValue(
+            UserProperty(name = "str", value = "12.3", type = UserPropertyType.STRING),
+            UserPropertyType.SEMVER, "12", ConditionOperator.Equal)
+        )
+    }
 
     @Test
     fun shouldCompareInteger() {
-        assertEquals(true, comparePropWithConditionValue(this.intProp, "100 ", ConditionOperator.Equal))
-        assertEquals(false, comparePropWithConditionValue(this.intProp, "100", ConditionOperator.NotEqual))
-        assertEquals(true, comparePropWithConditionValue(this.intProp, "200", ConditionOperator.NotEqual))
-        assertEquals(true, comparePropWithConditionValue(this.intProp, "90", ConditionOperator.GreaterThanOrEqual))
-        assertEquals(true, comparePropWithConditionValue(this.intProp, "110", ConditionOperator.LessThanOrEqual))
+        assertEquals(true, comparePropWithConditionValue(this.intProp, null, "100 ", ConditionOperator.Equal))
+        assertEquals(false, comparePropWithConditionValue(this.intProp, null, "100", ConditionOperator.NotEqual))
+        assertEquals(true, comparePropWithConditionValue(this.intProp, null, "200", ConditionOperator.NotEqual))
+        assertEquals(true, comparePropWithConditionValue(this.intProp, null, "90", ConditionOperator.GreaterThanOrEqual))
+        assertEquals(true, comparePropWithConditionValue(this.intProp, null, "110", ConditionOperator.LessThanOrEqual))
 
-        assertEquals(true, comparePropWithConditionValue(this.intProp, "10, 50, 100", ConditionOperator.In))
-        assertEquals(false, comparePropWithConditionValue(this.intProp, "10, 50, 99, 120", ConditionOperator.In))
-        assertEquals(true, comparePropWithConditionValue(this.intProp, "10, 50, 90", ConditionOperator.NotIn))
-        assertEquals(true, comparePropWithConditionValue(this.intProp, "99, 101", ConditionOperator.Between))
-        assertEquals(false, comparePropWithConditionValue(this.intProp, "98, 99", ConditionOperator.Between))
-        assertEquals(false, comparePropWithConditionValue(this.intProp, "98", ConditionOperator.Between))
+        assertEquals(true, comparePropWithConditionValue(this.intProp, null, "10, 50, 100", ConditionOperator.In))
+        assertEquals(false, comparePropWithConditionValue(this.intProp, null, "10, 50, 99, 120", ConditionOperator.In))
+        assertEquals(true, comparePropWithConditionValue(this.intProp, null, "10, 50, 90", ConditionOperator.NotIn))
+        assertEquals(true, comparePropWithConditionValue(this.intProp, null, "99, 101", ConditionOperator.Between))
+        assertEquals(false, comparePropWithConditionValue(this.intProp, null, "98, 99", ConditionOperator.Between))
+        assertEquals(false, comparePropWithConditionValue(this.intProp, null, "98", ConditionOperator.Between))
     }
 
     @Test
     fun shouldCompareDouble() {
-        assertEquals(true, comparePropWithConditionValue(this.doubleProp, "12.3 ", ConditionOperator.Equal))
-        assertEquals(false, comparePropWithConditionValue(this.doubleProp, "12.3", ConditionOperator.NotEqual))
-        assertEquals(true, comparePropWithConditionValue(this.doubleProp, "20.0", ConditionOperator.NotEqual))
-        assertEquals(true, comparePropWithConditionValue(this.doubleProp, "10", ConditionOperator.GreaterThanOrEqual))
-        assertEquals(true, comparePropWithConditionValue(this.doubleProp, "13", ConditionOperator.LessThanOrEqual))
+        assertEquals(true, comparePropWithConditionValue(this.doubleProp, null, "12.3 ", ConditionOperator.Equal))
+        assertEquals(false, comparePropWithConditionValue(this.doubleProp, null, "12.3", ConditionOperator.NotEqual))
+        assertEquals(true, comparePropWithConditionValue(this.doubleProp, null, "20.0", ConditionOperator.NotEqual))
+        assertEquals(true, comparePropWithConditionValue(this.doubleProp, null, "10", ConditionOperator.GreaterThanOrEqual))
+        assertEquals(true, comparePropWithConditionValue(this.doubleProp, null, "13", ConditionOperator.LessThanOrEqual))
 
-        assertEquals(true, comparePropWithConditionValue(this.doubleProp, "10, 10.2, 12.3", ConditionOperator.In))
-        assertEquals(false, comparePropWithConditionValue(this.doubleProp, "0.0, 11.1, 11, 12", ConditionOperator.In))
-        assertEquals(true, comparePropWithConditionValue(this.doubleProp, "0.0, 11.1, 11, 12", ConditionOperator.NotIn))
-        assertEquals(true, comparePropWithConditionValue(this.doubleProp, "0, 20", ConditionOperator.Between))
-        assertEquals(false, comparePropWithConditionValue(this.doubleProp, "30, 40", ConditionOperator.Between))
-        assertEquals(false, comparePropWithConditionValue(this.doubleProp, "1", ConditionOperator.Between))
+        assertEquals(true, comparePropWithConditionValue(this.doubleProp, null, "10, 10.2, 12.3", ConditionOperator.In))
+        assertEquals(false, comparePropWithConditionValue(this.doubleProp, null, "0.0, 11.1, 11, 12", ConditionOperator.In))
+        assertEquals(true, comparePropWithConditionValue(this.doubleProp, null, "0.0, 11.1, 11, 12", ConditionOperator.NotIn))
+        assertEquals(true, comparePropWithConditionValue(this.doubleProp, null, "0, 20", ConditionOperator.Between))
+        assertEquals(false, comparePropWithConditionValue(this.doubleProp, null, "30, 40", ConditionOperator.Between))
+        assertEquals(false, comparePropWithConditionValue(this.doubleProp, null, "1", ConditionOperator.Between))
     }
 
     @Test
     fun shouldCompareString() {
-        assertEquals(true, comparePropWithConditionValue(this.strProp, "[a-zA-Z]+", ConditionOperator.Regex))
-        assertEquals(false, comparePropWithConditionValue(this.strProp, "[0-9]+", ConditionOperator.Regex))
+        assertEquals(true, comparePropWithConditionValue(this.strProp, null, "[a-zA-Z]+", ConditionOperator.Regex))
+        assertEquals(false, comparePropWithConditionValue(this.strProp, null, "[0-9]+", ConditionOperator.Regex))
 
-        assertEquals(true, comparePropWithConditionValue(this.strProp, "Hello", ConditionOperator.Equal))
-        assertEquals(false, comparePropWithConditionValue(this.strProp, "Hello", ConditionOperator.NotEqual))
-        assertEquals(true, comparePropWithConditionValue(this.strProp, "Hello ", ConditionOperator.NotEqual))
+        assertEquals(true, comparePropWithConditionValue(this.strProp, null, "Hello", ConditionOperator.Equal))
+        assertEquals(false, comparePropWithConditionValue(this.strProp, null, "Hello", ConditionOperator.NotEqual))
+        assertEquals(true, comparePropWithConditionValue(this.strProp, null, "Hello ", ConditionOperator.NotEqual))
 
-        assertEquals(true, comparePropWithConditionValue(this.strProp, "Hello,Hello World", ConditionOperator.In))
-        assertEquals(true, comparePropWithConditionValue(this.strProp, "X,Y,Z", ConditionOperator.NotIn))
+        assertEquals(true, comparePropWithConditionValue(this.strProp, null, "Hello,Hello World", ConditionOperator.In))
+        assertEquals(true, comparePropWithConditionValue(this.strProp, null, "X,Y,Z", ConditionOperator.NotIn))
     }
 
     @Test
     fun shouldCompareSemver() {
-        assertEquals(true, comparePropWithConditionValue(this.semverProp, "1", ConditionOperator.Equal))
-        assertEquals(true, comparePropWithConditionValue(this.semverProp, "1.1", ConditionOperator.Equal))
-        assertEquals(true, comparePropWithConditionValue(this.semverProp, "1.1.1", ConditionOperator.Equal))
+        assertEquals(true, comparePropWithConditionValue(this.semverProp, null, "1", ConditionOperator.Equal))
+        assertEquals(true, comparePropWithConditionValue(this.semverProp, null, "1.1", ConditionOperator.Equal))
+        assertEquals(true, comparePropWithConditionValue(this.semverProp, null, "1.1.1", ConditionOperator.Equal))
     }
 
     @Test
     fun shouldCompareTimestamp() {
-        assertEquals(true, comparePropWithConditionValue(this.timeProp, "2011-10-05T14:48:00.000Z", ConditionOperator.Equal))
-        assertEquals(false, comparePropWithConditionValue(this.timeProp, "2011-10-05T14:49:00.000Z", ConditionOperator.Equal))
-        assertEquals(true, comparePropWithConditionValue(this.timeProp, "2011-10-05T14:47:00.000Z", ConditionOperator.GreaterThanOrEqual))
-        assertEquals(true, comparePropWithConditionValue(this.timeProp, "2011-10-05T14:49:00.000Z", ConditionOperator.LessThanOrEqual))
+        assertEquals(true, comparePropWithConditionValue(this.timeProp, null, "2011-10-05T14:48:00.000Z", ConditionOperator.Equal))
+        assertEquals(false, comparePropWithConditionValue(this.timeProp, null, "2011-10-05T14:49:00.000Z", ConditionOperator.Equal))
+        assertEquals(true, comparePropWithConditionValue(this.timeProp, null, "2011-10-05T14:47:00.000Z", ConditionOperator.GreaterThanOrEqual))
+        assertEquals(true, comparePropWithConditionValue(this.timeProp, null, "2011-10-05T14:49:00.000Z", ConditionOperator.LessThanOrEqual))
+    }
+
+    @Test
+    fun shouldCompareBoolean() {
+        assertEquals(true, comparePropWithConditionValue(this.boolProp, null, "false", ConditionOperator.Equal))
+        assertEquals(true, comparePropWithConditionValue(this.boolProp, null, "true", ConditionOperator.NotEqual))
+    }
+
+    @Test
+    fun shouldParseStringToBoolean() {
+        assertEquals(false, parseStringToBoolean("false"))
+        assertEquals(false, parseStringToBoolean("False"))
+        assertEquals(false, parseStringToBoolean("FALSE"))
+        assertEquals(false, parseStringToBoolean("No"))
+        assertEquals(false, parseStringToBoolean("nil"))
+        assertEquals(false, parseStringToBoolean("off"))
+        assertEquals(false, parseStringToBoolean("off"))
     }
 }
 
