5c942330db7ab400bff22dff2d1073b93431353a
RyosukeCla <RyosukeCla@users.noreply.github.com>
2025-08-01 15:24:52 +0900

    chore: update SDK version to 0.11.0 in podspec and sdk.swift

--- ios/Nativebrik/Nativebrik/sdk.swift
+++ ios/Nativebrik/Nativebrik/sdk.swift
@@ -12,7 +12,7 @@ import Combine
 // for development
 public var nativebrikTrackUrl = "https://track.nativebrik.com/track/v1"
 public var nativebrikCdnUrl = "https://cdn.nativebrik.com"
-public let nativebrikSdkVersion = "0.10.2"
+public let nativebrikSdkVersion = "0.11.0"
 
 public let isNativebrikAvailable: Bool = {
     if #available(iOS 15.0, *) {
