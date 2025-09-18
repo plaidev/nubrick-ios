e5c614c79176e97e882c8e7bea1428b40ba8346e
t-jimbo <jbear.um@gmail.com>
2025-09-18 18:20:59 +0900

    feat: add pod tools

---
+++ ios/xcframeworks/PodBuilder/PodBuilder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PodBuilder
+//
+//  Created by Takuma Jimbo on 2025/09/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/xcframeworks/PodBuilder/PodBuilder/PodBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodBuilderApp.swift
+//  PodBuilder
+//
+//  Created by Takuma Jimbo on 2025/09/18.
+//
+
+import SwiftUI
+
+@main
+struct PodBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
