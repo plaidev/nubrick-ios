240252ad632d5e89e93ecbd04da6bb363ee90cc2
t-jimbo <jbear.um@gmail.com>
2025-09-18 14:50:01 +0900

    ci: fix test run

--- ios/Package.swift
+++ ios/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/nalexn/ViewInspector", from: "0.10.2")
+        .package(url: "https://github.com/nalexn/ViewInspector", exact: "0.9.11")
     ],
     targets: [
         .target(
