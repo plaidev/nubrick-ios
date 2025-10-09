609c4ccd5b90f72b200c551f38d2fff44789f0da
t-jimbo <jbear.um@gmail.com>
2025-10-09 18:12:08 +0900

    chore: update h file

--- Sources/Nubrick/Nubrick.h
+++ Sources/Nubrick/Nubrick.h
@@ -7,12 +7,10 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for Nativebrik.
-//FOUNDATION_EXPORT double NativebrikVersionNumber;
+//! Project version number for Nubrick.
+//FOUNDATION_EXPORT double NubrickVersionNumber;
 
-//! Project version string for Nativebrik.
-//FOUNDATION_EXPORT const unsigned char NativebrikVersionString[];
+//! Project version string for Nubrick.
+//FOUNDATION_EXPORT const unsigned char NubrickVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Nativebrik/PublicHeader.h>
-
-
+// In this header, you should import all the public headers of your framework using statements like #import <Nubrick/PublicHeader.h>
