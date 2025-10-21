49595992bafbbaf50905060325c2da098459d884
t-jimbo <jbear.um@gmail.com>
2025-10-21 13:22:27 +0900

    feat: create new project

---
+++ Examples/Example-CocoaPods/Example-CocoaPods/Example_CocoaPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_CocoaPodsApp.swift
+//  Example-CocoaPods
+//
+//  Created by Takuma Jimbo on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Example_CocoaPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
