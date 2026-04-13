885e5bbb3684b37c0d7709e33e9dd47fcd53f4af
Victor Bayona <victor.bayona@plaid.co.jp>
2026-04-13 15:13:26 +0900

    Remove custom endpoints and move version to project settings

--- Examples/Example-CocoaPods/Example-CocoaPods/Example_CocoaPodsApp.swift
+++ Examples/Example-CocoaPods/Example-CocoaPods/Example_CocoaPodsApp.swift
@@ -24,15 +24,8 @@ struct ExampleApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     init() {
-        let cdnUrl = (Bundle.main.object(forInfoDictionaryKey: "CDN_URL") as? String)?
-            .trimmingCharacters(in: .whitespacesAndNewlines)
-        let trackUrl = (Bundle.main.object(forInfoDictionaryKey: "TRACK_URL") as? String)?
-            .trimmingCharacters(in: .whitespacesAndNewlines)
-
         NubrickSDK.initialize(
             projectId: "cgv3p3223akg00fod19g",
-            trackUrl: (trackUrl?.isEmpty == false) ? trackUrl : nil,
-            cdnUrl: (cdnUrl?.isEmpty == false) ? cdnUrl : nil,
             cachePolicy: NubrickCachePolicy(cacheTime: 10 * 60, staleTime: 0)
         )
     }
