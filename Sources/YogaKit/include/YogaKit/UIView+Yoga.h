9b46e81b81eeeef78b3bb3c10e7059d418bfa59f
Victor Bayona <victor.bayona@plaid.co.jp>
2026-08-12 17:25:34 +0900

    Add license notices to framework artifacts

--- Sources/YogaKit/include/YogaKit/UIView+Yoga.h
+++ Sources/YogaKit/include/YogaKit/UIView+Yoga.h
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the MIT license. The full license text
+ * is in THIRD-PARTY-NOTICES at the repository root.
  */
 
 #import <UIKit/UIKit.h>
