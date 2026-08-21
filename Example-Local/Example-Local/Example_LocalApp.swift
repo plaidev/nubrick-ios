f78035e3103e21a868854d53819966ce566db6c1
Victor Bayona <62053428+VictorB00@users.noreply.github.com>
2026-08-21 15:57:03 +0900

    Simply Example-Local app (#460)

--- Example-Local/Example-Local/Example_LocalApp.swift
+++ Example-Local/Example-Local/Example_LocalApp.swift
@@ -1,29 +1,14 @@
 import Nubrick
 import SwiftUI
-import UIKit
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        return true
-    }
-}
 
 @main
 @MainActor
 struct ExampleApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
     init() {
         guard let projectId = Bundle.main.object(forInfoDictionaryKey: "PROJECT_ID") as? String else {
             fatalError("Missing or invalid PROJECT_ID in Info.plist")
         }
-
-        NubrickSDK.initialize(
-            projectId: projectId
-        )
+        NubrickSDK.initialize(projectId: projectId)
     }
 
     var body: some Scene {
